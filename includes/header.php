fda2e86f4f192db87d91fb201857d243ec25c538
Annie Fu <af397@cornell.edu>
2020-03-01 12:09:46 -0500

    changes

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,13 @@
 <div class="header_back">
-<figure class="logo">
-<img id ="logo" src="images/vr_logo.png" alt="logo" />
-
-</figure>
 <nav id='nav'>
 
 <ul>
-<li><a href="index.php">HOME</a></li>
+<!-- <li><a href="index.php">HOME</a></li> -->
 <li><a href="review.php">EXPLORE</a></li>
+<li class="logo">
+<img id ="logo" src="images/vr_logo.png" alt="logo" />
+
+</li>
 <li> <a href="member_portal.php">MY ACCOUNT</a></li>
 
 </ul>
